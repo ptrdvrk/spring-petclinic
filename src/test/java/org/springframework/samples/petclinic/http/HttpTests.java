0c7d7abe63a4a4c39f8021978d0f7624227ef394
Parth Mangroliya <parth.mangroliya@kuliza.com>
2020-09-29 20:42:25 +0530

    strict https check removed, refactoring testcase

--- src/test/java/org/springframework/samples/petclinic/http/HttpTests.java
+++ src/test/java/org/springframework/samples/petclinic/http/HttpTests.java
@@ -8,10 +8,11 @@ import java.net.URL;
 
 public class HttpTests {
 
-    @Test
-    public void http_POC() throws IOException {
-        URL url = new URL("http://www.google.com");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.connect();
-    }
+	@Test
+	public void http_POC() throws IOException {
+		URL url = new URL("http://www.google.com");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.connect();
+	}
+
 }
