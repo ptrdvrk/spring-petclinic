3dfa607274b732916922bcb336f7dab992d38e64
Parth Mangroliya <parth.mangroliya@kuliza.com>
2020-09-30 21:20:05 +0530

    different test scenarios for http calls

--- src/test/java/org/springframework/samples/petclinic/http/HttpTests.java
+++ src/test/java/org/springframework/samples/petclinic/http/HttpTests.java
@@ -9,10 +9,40 @@ import java.net.URL;
 public class HttpTests {
 
 	@Test
-	public void http_POC() throws IOException {
+	public void http_connect() throws IOException {
 		URL url = new URL("http://www.google.com");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.connect();
+	}
+
+	@Test
+	public void http_responseCode() throws IOException {
+		URL url = new URL("http://www.google.com");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.getResponseCode();
+	}
+
+	@Test
+	public void http_responseHeader() throws IOException {
+		URL url = new URL("http://www.google.com");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.getHeaderField(1);
+	}
+
+	@Test
+	public void http_contentType() throws IOException {
+		URL url = new URL("http://www.google.com");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.getContentType();
+	}
+
+	@Test
+	public void http() throws IOException {
+		URL url = new URL("http://www.google.com");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.connect();
+		connection.getResponseCode();
+		connection.getContentType();
 	}
 
 }
